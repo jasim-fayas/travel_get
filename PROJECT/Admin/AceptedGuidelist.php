55ca80afaf7ea4f76fdba2e9935ca4321665852d
jasim-fayas <jazzz7080@gmail.com>
2024-02-19 17:14:06 +0530

    first commit

---
+++ PROJECT/Admin/AceptedGuidelist.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<form id="form1" name="form1" method="post" action="">
+  <table width="786" height="170" border="1">
+    <tr>
+      <td>Sl number</td>
+      <td>Name</td>
+      <td>Contact</td>
+      <td>Email</td>
+      <td>Photo</td>
+      <td>Proof</td>
+      <td>Action</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+  </table>
+</form>
